cb7de5580d5c9a76cb0301a7c2ebec6c305dc8f5
GabrielEug2 <gabriel.eug2@gmail.com>
2018-05-28 19:55:52 -0300

    Lendo dados dos sensores e setando velocidade pra teste

--- mazeRemoteApi/src/RemoteApi.java
+++ mazeRemoteApi/src/RemoteApi.java
@@ -3,9 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.lang.Math;
+import coppelia.FloatW;
 import coppelia.FloatWA;
+import coppelia.FloatWAA;
 import coppelia.IntW;
+import coppelia.IntWA;
 import coppelia.remoteApi;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 
 /**
  *
@@ -35,8 +41,8 @@ public class RemoteApi {
             // inicialização do robo
             String robot_name = "bubbleRob";
             System.out.print("Procurando objeto " + robot_name + "...");
-            IntW robot_handle = new IntW(0);
-            if(vrep.simxGetObjectHandle(clientID, robot_name, robot_handle, vrep.simx_opmode_blocking) == vrep.simx_return_ok)
+            int robot_handle = 0;
+            if(vrep.simxGetObjectHandle(clientID, robot_name, new IntW(robot_handle), vrep.simx_opmode_blocking) == vrep.simx_return_ok)
                 System.out.println("Conectado! <<<<< " +
                     " depois temos que trocar pro nosso robô!");
             else {
@@ -46,30 +52,36 @@ public class RemoteApi {
             }
     
             // inicialização dos sensores
+            final int NUM_SENSORS = 5;
             System.out.print("Conectando-se aos sensores...");
-            IntW left_sensor = new IntW(0),
-                 lm_sensor = new IntW(0),
-                 middle_sensor = new IntW(0),
-                 rm_sensor = new IntW(0),
-                 right_sensor = new IntW(0);
-            if (vrep.simxGetObjectHandle(clientID, "Left_ultrasonic", left_sensor, vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
-                vrep.simxGetObjectHandle(clientID, "LM_ultrasonic", lm_sensor, vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
-                vrep.simxGetObjectHandle(clientID, "Middle_ultrasonic", middle_sensor, vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
-                vrep.simxGetObjectHandle(clientID, "RM_ultrasonic", rm_sensor, vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
-                vrep.simxGetObjectHandle(clientID, "Right_ultrasonic", right_sensor, vrep.simx_opmode_blocking) == vrep.simx_return_ok
+            IntWA sensors = new IntWA(NUM_SENSORS);
+            
+            if (vrep.simxGetObjectHandle(clientID, "Left_ultrasonic", new IntW(sensors.getArray()[0]), vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
+                vrep.simxGetObjectHandle(clientID, "LM_ultrasonic", new IntW(sensors.getArray()[1]), vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
+                vrep.simxGetObjectHandle(clientID, "Middle_ultrasonic", new IntW(sensors.getArray()[2]), vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
+                vrep.simxGetObjectHandle(clientID, "RM_ultrasonic", new IntW(sensors.getArray()[3]), vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
+                vrep.simxGetObjectHandle(clientID, "Right_ultrasonic", new IntW(sensors.getArray()[4]), vrep.simx_opmode_blocking) == vrep.simx_return_ok
                 )
                 System.out.println("Sucesso! (ultrassom)");
-            else if(vrep.simxGetObjectHandle(clientID, "Left_Vision_sensor", left_sensor, vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
-                vrep.simxGetObjectHandle(clientID, "LM_Vision_sensor", lm_sensor, vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
-                vrep.simxGetObjectHandle(clientID, "Middle_Vision_sensor", middle_sensor, vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
-                vrep.simxGetObjectHandle(clientID, "RM_Vision_sensor", rm_sensor, vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
-                vrep.simxGetObjectHandle(clientID, "Right_Vision_sensor", right_sensor, vrep.simx_opmode_blocking) == vrep.simx_return_ok
+            else if(vrep.simxGetObjectHandle(clientID, "Left_Vision_sensor", new IntW(sensors.getArray()[0]), vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
+                vrep.simxGetObjectHandle(clientID, "LM_Vision_sensor", new IntW(sensors.getArray()[1]), vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
+                vrep.simxGetObjectHandle(clientID, "Middle_Vision_sensor", new IntW(sensors.getArray()[2]), vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
+                vrep.simxGetObjectHandle(clientID, "RM_Vision_sensor", new IntW(sensors.getArray()[3]), vrep.simx_opmode_blocking) == vrep.simx_return_ok &&
+                vrep.simxGetObjectHandle(clientID, "Right_Vision_sensor", new IntW(sensors.getArray()[4]), vrep.simx_opmode_blocking) == vrep.simx_return_ok
                 )
                 System.out.println("Sucesso! (visão)");
             else {
                 System.out.println("Falhou!\nSem sensores não dá. Saindo...");
                 end_connection(vrep, clientID);
             }
+
+            FloatWAA sensors_read_values = new FloatWAA(NUM_SENSORS);
+            for (int i=0; i < NUM_SENSORS; i++)
+                sensors_read_values.getArray()[i] = new FloatWA(3);
+            
+            FloatWA distances = new FloatWA(NUM_SENSORS);
+            for (int i=0; i < NUM_SENSORS; i++)
+                distances.getArray()[i] = 0;
             
             // inicialização dos motores
             IntW left_motor_handle = new IntW(0),
@@ -89,35 +101,47 @@ public class RemoteApi {
                 end_connection(vrep, clientID);
             }
 
-            //loop de execucao
+            //outras variáveis 
             FloatWA position = new FloatWA(3), //x, y, z. Nao usa-se o z
                     angle = new FloatWA(3); //alpha, beta e gamma. Usa-se o gamma
             float current_x, current_y,
                   current_angle;
-            long time_limit = 2*NANOS_PER_S;
+
+            long time_limit = 10*NANOS_PER_S;
             System.out.println("  << Enquanto ele não se movimenta, roda só um pouco e depois para >> ");
             System.out.println("  << Depois podemos colocar um limite de tempo para ele desistir >> ");
             long startTime = System.nanoTime();
-        
-            vrep.simxGetObjectOrientation(clientID, robot_handle.getValue(), -1, angle, vrep.simx_opmode_streaming);
-            vrep.simxGetObjectPosition(clientID, robot_handle.getValue(), -1, position, vrep.simx_opmode_streaming);
 
+            //loop de execucao
             while (vrep.simxGetConnectionId(clientID) != -1 &&
                    System.nanoTime() - startTime < time_limit) {
                 //Lê posição e ângulo
-                vrep.simxGetObjectOrientation(clientID, robot_handle.getValue(), -1, angle, vrep.simx_opmode_streaming);
-                vrep.simxGetObjectPosition(clientID, robot_handle.getValue(), -1, position, vrep.simx_opmode_streaming);
+                vrep.simxGetObjectOrientation(clientID, robot_handle, -1, angle, vrep.simx_opmode_streaming);
+                vrep.simxGetObjectPosition(clientID, robot_handle, -1, position, vrep.simx_opmode_streaming);
                 current_x = position.getArray()[0];
                 current_y = position.getArray()[1];
                 current_angle = angle.getArray()[2];
 
-                System.out.format("Posicao (x,y): %.2f %.2f)\n", current_x, current_y);
-                System.out.format("Angulo em radianos: %.3f\n", current_angle);
+//                System.out.format("Posicao (x,y): %.2f %.2f)\n", current_x, current_y);
+//                System.out.format("Angulo em radianos: %.3f\n", current_angle);
                 
-                // Pega os valores dos sensores
-                    // Qual o tipo dos valores retornados?
+                for(int i=0; i < NUM_SENSORS; i++) {
+                    vrep.simxReadProximitySensor(clientID, sensors.getArray()[i], null, sensors_read_values.getArray()[i],null,null,vrep.simx_opmode_streaming);
+                    distances.getArray()[i] =
+                        (float) pow(sensors_read_values.getArray()[i].getArray()[0], 2) +
+                        (float) pow(sensors_read_values.getArray()[i].getArray()[1], 2) +
+                        (float) pow(sensors_read_values.getArray()[i].getArray()[2], 2);
+                    distances.getArray()[i] = (float) sqrt(distances.getArray()[i]);
+                }
+     
+                
                 // Faz inferência fuzzy pra calcular a velocidade de cada motor
+                
+                
                 // Seta a velocidade em cada motor
+                vrep.simxSetJointTargetVelocity(clientID,left_motor_handle.getValue(), (float)15, vrep.simx_opmode_oneshot);
+                vrep.simxSetJointTargetVelocity(clientID,right_motor_handle.getValue(), (float)15, vrep.simx_opmode_oneshot);
+
             }
             
             System.out.println("(Conexão fechada) Encerrando...");
