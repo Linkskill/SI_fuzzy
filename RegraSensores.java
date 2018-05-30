2ca0f7360fad5d690ede3b20ac63f454808933e8
Lincoln <linkskill@gmail.com>
2018-05-30 20:12:10 -0300

    Alterando RegraSensores (wallDetection)

--- RegraSensores.java
+++ RegraSensores.java
@@ -1,5 +1,5 @@
 Engine engine = new Engine();
-engine.setName("");
+engine.setName("wallDetection");
 
 InputVariable inputVariable1 = new InputVariable();
 inputVariable1.setEnabled(true);
@@ -28,61 +28,34 @@ inputVariable3.addTerm(new Trapezoid("Medio", 0.050, 0.080, 0.120, 0.150));
 inputVariable3.addTerm(new Trapezoid("Longe", 0.125, 0.150, 0.200, 0.200));
 engine.addInputVariable(inputVariable3);
 
-InputVariable inputVariable4 = new InputVariable();
-inputVariable4.setEnabled(true);
-inputVariable4.setName("SensorDiagEsq");
-inputVariable4.setRange(0.000, 0.200);
-inputVariable4.addTerm(new Trapezoid("Perto", 0.000, 0.000, 0.050, 0.075));
-inputVariable4.addTerm(new Trapezoid("Medio", 0.050, 0.080, 0.120, 0.150));
-inputVariable4.addTerm(new Trapezoid("Longe", 0.125, 0.150, 0.200, 0.200));
-engine.addInputVariable(inputVariable4);
-
-InputVariable inputVariable5 = new InputVariable();
-inputVariable5.setEnabled(true);
-inputVariable5.setName("SensorDiagDir");
-inputVariable5.setRange(0.000, 0.200);
-inputVariable5.addTerm(new Trapezoid("Perto", 0.000, 0.000, 0.050, 0.075));
-inputVariable5.addTerm(new Trapezoid("Medio", 0.050, 0.080, 0.120, 0.150));
-inputVariable5.addTerm(new Trapezoid("Longe", 0.125, 0.150, 0.200, 0.200));
-engine.addInputVariable(inputVariable5);
-
-InputVariable inputVariable6 = new InputVariable();
-inputVariable6.setEnabled(true);
-inputVariable6.setName("SensorFrente2");
-inputVariable6.setRange(0.000, 0.200);
-inputVariable6.addTerm(new Trapezoid("Perto", 0.000, 0.000, 0.050, 0.075));
-inputVariable6.addTerm(new Trapezoid("Medio", 0.050, 0.080, 0.120, 0.150));
-inputVariable6.addTerm(new Trapezoid("Longe", 0.125, 0.150, 0.200, 0.200));
-engine.addInputVariable(inputVariable6);
-
 OutputVariable outputVariable1 = new OutputVariable();
 outputVariable1.setEnabled(true);
 outputVariable1.setName("MotorEsq");
-outputVariable1.setRange(-0.200, 0.200);
+outputVariable1.setRange(-1.000, 1.000);
 outputVariable1.fuzzyOutput().setAccumulation(new AlgebraicSum());
 outputVariable1.setDefuzzifier(new Centroid(200));
-outputVariable1.setDefaultValue(0.200);
+outputVariable1.setDefaultValue(0.000);
 outputVariable1.setLockValidOutput(false);
 outputVariable1.setLockOutputRange(true);
-outputVariable1.addTerm(new Trapezoid("Reverso", -0.200, -0.200, -0.100, 0.000));
-outputVariable1.addTerm(new Trapezoid("Fraco", 0.000, 0.000, 0.050, 0.075));
-outputVariable1.addTerm(new Trapezoid("Media", 0.050, 0.075, 0.125, 0.150));
-outputVariable1.addTerm(new Trapezoid("Forte", 0.125, 0.150, 0.200, 0.200));
+outputVariable1.addTerm(new Trapezoid("ReversoRapido", -1.000, -1.000, -0.600, -0.400));
+outputVariable1.addTerm(new Trapezoid("ReversoLento", -0.600, -0.400, 0.000, 0.000));
+outputVariable1.addTerm(new Trapezoid("Lento", 0.000, 0.000, 0.400, 0.600));
+outputVariable1.addTerm(new Trapezoid("Rapido", 0.400, 0.600, 1.000, 1.000));
 engine.addOutputVariable(outputVariable1);
 
 OutputVariable outputVariable2 = new OutputVariable();
 outputVariable2.setEnabled(true);
 outputVariable2.setName("MotorDir");
-outputVariable2.setRange(-0.200, 0.200);
+outputVariable2.setRange(-1.000, 1.000);
 outputVariable2.fuzzyOutput().setAccumulation(new AlgebraicSum());
 outputVariable2.setDefuzzifier(new Centroid(200));
-outputVariable2.setDefaultValue(0.200);
+outputVariable2.setDefaultValue(0.000);
 outputVariable2.setLockValidOutput(false);
 outputVariable2.setLockOutputRange(true);
-outputVariable2.addTerm(new Trapezoid("Reverso", -0.200, -0.200, -0.100, 0.000));
-outputVariable2.addTerm(new Trapezoid("Fraco", 0.000, 0.000, 0.050, 0.075));
-outputVariable2.addTerm(new Trapezoid("Media", 0.050, 0.075, 0.125, 0.150));
-outputVariable2.addTerm(new Trapezoid("Forte", 0.125, 0.150, 0.200, 0.200));
+outputVariable2.addTerm(new Trapezoid("ReversoRapido", -1.000, -1.000, -0.600, -0.400));
+outputVariable2.addTerm(new Trapezoid("ReversoLento", -0.600, -0.400, 0.000, 0.000));
+outputVariable2.addTerm(new Trapezoid("Lento", 0.000, 0.000, 0.400, 0.600));
+outputVariable2.addTerm(new Trapezoid("Rapido", 0.400, 0.600, 1.000, 1.000));
 engine.addOutputVariable(outputVariable2);
 
 RuleBlock ruleBlock = new RuleBlock();
@@ -91,14 +64,14 @@ ruleBlock.setName("");
 ruleBlock.setConjunction(new Minimum());
 ruleBlock.setDisjunction(new Maximum());
 ruleBlock.setActivation(new Minimum());
-ruleBlock.addRule(Rule.parse("if SensorFrente is Longe or SensorFrente2 is Longe then MotorDir is Forte and MotorEsq is Forte", engine));
-ruleBlock.addRule(Rule.parse("if SensorFrente is Medio or SensorFrente2 is Medio then MotorDir is Media and MotorEsq is Media", engine));
-ruleBlock.addRule(Rule.parse("if SensorFrente is Perto or SensorFrente2 is Perto and SensorDir is Perto or SensorDiagDir is Perto and SensorEsq is Perto or SensorDiagEsq is Perto then MotorDir is Forte and MotorEsq is Reverso", engine));
-ruleBlock.addRule(Rule.parse("if SensorEsq is Medio or SensorDiagEsq is Medio then MotorEsq is Forte and MotorDir is Media", engine));
-ruleBlock.addRule(Rule.parse("if SensorDir is Medio or SensorDiagDir is Medio then MotorDir is Forte and MotorEsq is Media", engine));
-ruleBlock.addRule(Rule.parse("if SensorEsq is Perto or SensorDiagEsq is Perto then MotorEsq is Forte and MotorDir is Fraco", engine));
-ruleBlock.addRule(Rule.parse("if SensorDir is Perto or SensorDiagDir is Perto then MotorDir is Forte and MotorEsq is Fraco", engine));
-ruleBlock.addRule(Rule.parse("if SensorDir is Medio or SensorDiagDir is Medio and SensorEsq is Medio or SensorDiagEsq is Medio then MotorDir is Media and MotorEsq is Media", engine));
+ruleBlock.addRule(Rule.parse("if SensorFrente is Longe then MotorDir is Rapido and MotorEsq is Rapido", engine));
+ruleBlock.addRule(Rule.parse("if SensorFrente is Medio then MotorDir is Lento and MotorEsq is Lento", engine));
+ruleBlock.addRule(Rule.parse("if SensorFrente is Perto and SensorDir is Perto and SensorEsq is Perto then MotorDir is Rapido and MotorEsq is ReversoRapido", engine));
+ruleBlock.addRule(Rule.parse("if SensorEsq is Medio then MotorEsq is Rapido and MotorDir is Lento", engine));
+ruleBlock.addRule(Rule.parse("if SensorDir is Medio then MotorDir is Rapido and MotorEsq is Lento", engine));
+ruleBlock.addRule(Rule.parse("if SensorEsq is Perto then MotorEsq is Rapido and MotorDir is ReversoLento", engine));
+ruleBlock.addRule(Rule.parse("if SensorDir is Perto then MotorDir is Rapido and MotorEsq is ReversoLento", engine));
+ruleBlock.addRule(Rule.parse("if SensorDir is Medio and SensorEsq is Medio then MotorDir is Lento and MotorEsq is Lento", engine));
 engine.addRuleBlock(ruleBlock);
 
 
